502968bd5962099043139094cbfdbf55cc47f092
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2007-09-15 23:50:19 +0000

    updated pdcgi to not use a single read() and lose data...
    updated trydirent for compiler warnings.
    
    added caltime_local()
    
    
    
    
    git-svn-id: file:///source/coreland/chrono/trunk@2780 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- calt_loc.c
+++ calt_loc.c
@@ -9,18 +9,21 @@
 
 /* XXX: localtime() ... */
 
-void caltime_local(struct caltime *ct, const struct tai *t)
+void caltime_local(struct caltime *ct, const struct tai *t, int *wday, int *yday)
 {
   struct tm *tm;
   uint64 u = tai_get(t) - TAI_1970;
   time_t tt = (time_t) u;
 
   tm = localtime(&tt);
-  ct->offset = tm->tm_gmtoff;
+  ct->offset = tm->tm_gmtoff / 60;
   ct->second = tm->tm_sec;
   ct->minute = tm->tm_min;
   ct->hour = tm->tm_hour;
   ct->date.year = tm->tm_year + 1900;
   ct->date.month = tm->tm_mon;
-  ct->date.day = tm->tm_yday;
+  ct->date.day = tm->tm_mday;
+
+  if (wday) *wday = tm->tm_wday;
+  if (yday) *yday = tm->tm_yday;
 }
--- caltime.h
+++ caltime.h
@@ -17,7 +17,7 @@ struct caltime {
 
 void caltime_tai(const struct caltime *, struct tai *);
 void caltime_utc(struct caltime *, const struct tai *, int *, int *);
-void caltime_local(struct caltime *, const struct tai *);
+void caltime_local(struct caltime *, const struct tai *, int *, int *);
 
 unsigned int caltime_fmt(char *, const struct caltime *);
 unsigned int caltime_scan(const char *, struct caltime *);
